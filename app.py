84abb73654a7fb4d8275c3764a58f460393b39a6
gorretgolden <nabatanzigorret143@gmail.com>
2025-02-05 14:20:57 +0300

    added docker-compose file

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
+#importing  required modules and packages
 from flask import Flask, render_template
 
+#creating a new flask app instance
 app = Flask(__name__)
 
+
+#definig a route for the index page
 @app.route('/')
 def home():
     return render_template('index.html')
