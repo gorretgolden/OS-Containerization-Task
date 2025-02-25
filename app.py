2e3b10d15e0e08f5dd908b0211a52425be0b44bc
gorretgolden <nabatanzigorret143@gmail.com>
2025-02-25 17:00:16 +0300

    updated the readme file and app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #importing  required modules and packages
-from flask import Flask, render_template
+from flask import Flask
 
 #creating a new flask app instance
 app = Flask(__name__)
@@ -8,7 +8,7 @@ app = Flask(__name__)
 #definig a route for the index page
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return 'OS Task'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0', port=5000)
